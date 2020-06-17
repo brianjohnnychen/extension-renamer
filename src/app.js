de883cf9eebe1ade31c57de15526a7f7af049629
Brian Chen <brianjohnnychen@gmail.com>
2020-06-16 23:19:11 -0400

    Added user instructions.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,22 @@
+// CONFIGURE DESIRED EXTENSION
+const newExtension = '.png'
+
+// CONFIGURE DIRECTORY CONTAINING FILES TO BE RENAMED.
+const folder = '../extension-renamer/to_rename/'
+
+
+
+
+
+
+
+
+
+
+
+/** DO NOT CHANGE CONTENTS BELOW THIS LINE */
 const fs = require('fs')
 const path = require('path')
-
-// fs.rename('old file path', 'new file path', (err) => {})
-
-// Folder path containing files to be renamed.
-const folder = '../extension-renamer/to_rename/'
-const newExtension = '.txt'
 
 // Read contents of folder.
 fs.readdir(folder, (err, files) => {
